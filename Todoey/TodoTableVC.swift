28975a4936be6477c9a4c4f6289d72349fa284a7
Aphaki <kiapha1108@gmail.com>
2023-04-06 18:43:14 +0900

    add TableVC Data source, Delegate

--- Todoey/TodoTableVC.swift
+++ Todoey/TodoTableVC.swift
@@ -24,14 +24,30 @@ class TodoTableVC: UITableViewController {
     
     //MARK: - Table View Data Source
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
         
-        var cell = UITableViewCell(style: .default, reuseIdentifier: "TodoListContext")
-        
-        return
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = self.myToDoContext[indexPath.row]
+            cell.contentConfiguration = content
+        } else {
+            cell.textLabel?.text = self.myToDoContext[indexPath.row]
+        }
+        return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return myToDoContext.count
+    }
+    
+    //MARK: - Table View Delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        if cell?.accessoryType == .checkmark {
+            cell?.accessoryType = .none
+        } else {
+            cell?.accessoryType = .checkmark
+        }
     }
 
 
